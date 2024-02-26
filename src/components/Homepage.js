e94e7344ce78a575047db832f1a0083de9ce4960
soundarya788 <soundaryaa040@gmail.com>
2024-02-26 20:47:13 +0530

    update

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -20,23 +20,30 @@ function Homepage() {
 
 
   return (
+
+    <div className='gradient'>
+       
     <div className='term'>
+        <h2 style={{textAlign:'center'}} className='rules'>Rules & Regulations </h2>
+        <ul>
+            <li>Each participant will have 5 minutes to complete the quiz.</li>
+            <li>The quiz will consist of 5 multiple-choice questions.</li>
+            <li>Each question has a single correct answer.</li>
+            <li>Participants must complete the quiz within the time limit.</li>
+            <li>The quiz moderator's decisions on scoring and timing are final.</li>
+            <li>Fair play and adherence to quiz guidelines are expected from all participants.</li>
+            
+        </ul>
+    
         <div className='agree'>
         
-        <input type="checkbox" checked={conditions} onChange={handleChange} />
-
-        
-       <label>I agree terms and conditions</label>
-       
-        
-        <div className='button' >
-        <button  onClick={handleStart} disabled={!conditions}>
-          Agree
+         <input type="checkbox" checked={conditions} onChange={handleChange} />
+          <label>I agree terms and conditions</label>
+        <div className='box' >
+        <button className="button"onClick={handleStart} disabled={!conditions}>
+          Continue
         </button>
-        </div>
-
-        </div>
-
+        </div> </div> </div>
     </div>
   );
   }
